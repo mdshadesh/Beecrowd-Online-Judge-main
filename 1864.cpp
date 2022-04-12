bbb8a4c66050cedf9bd68157fc77bb765ca173a9
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2022-04-12 22:19:54 +0600

    Add files via upload

---
+++ 1864.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    char ara[40]="LIFE IS NOT A PROBLEM TO BE SOLVED";
+    scanf("%d", &a);
+    for(b=0; b<a; b++)
+        printf("%c", ara[b]);
+    printf("\n");
+    return 0;
+}
