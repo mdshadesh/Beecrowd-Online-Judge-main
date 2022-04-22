9bb53a69e4713e40b372bd16c907a7a0bc63ad38
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2022-04-22 20:40:26 +0600

    Add files via upload

---
+++ 2172.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long int x, m;
+    while(1)
+    {
+        scanf("%lld%lld", &x, &m);
+        if(x==0 && m==0) 
+        break;
+        printf("%lld\n", x*m);
+    }
+    return 0;
+}
---
+++ 2234.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    double M, P;
+    scanf("%lf %lf", &M, &P);
+    printf("%.2lf\n", M/P);
+    return 0;
+}
