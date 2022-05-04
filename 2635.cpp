43be5f7b9d6756ab39a6352ede7e01b8ec0de067
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2022-05-04 14:16:57 +0600

    Add files via upload

---
+++ 2635.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, q, wc, l;
+    string s;
+    while (cin >> n)
+    {
+        wc=-1;
+        l=0;
+        vector<string>v, in, a;
+        while (n--)
+        {
+            cin >> s;
+            v.push_back(s);
+        }
+        cin >> q;
+        while (q--)
+        {
+            cin >> s;
+
+        }
+        cout << wc << endl;
+        break;
+    }
+}
