8e2209fba499391bd2b67d9349011739f47e4bb0
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2023-04-24 23:04:38 +0600

    Add files via upload

---
+++ 3299.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace _3299_Numeros_Má_Sorte_Pequenos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Convert.ToString(Console.ReadLine());
+
+            if (num.Contains("13")) {
+
+                Console.WriteLine("{0} es de Mala Suerte",num);
+            }
+            else {
+
+                Console.WriteLine("{0} NO es de Mala Suerte",num);
+            }
+
+    
+        }    
+    }
+}
